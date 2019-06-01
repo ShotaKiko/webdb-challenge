e51bae0de81e5d16929b1fc392a0914fe0aa9217
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-01 11:42:33 -0500

    added knexfile

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex')
 
+const config = {
+    client: "sqlite3",
+    connection: {
+      filename : './data/webdb_sprint.sqlite3'
+    },
+    useNullasDefault: true
+};
+
+const db = knex(config)
+
 const server = express();
 server.use(express.json());
 server.use(helmet());
