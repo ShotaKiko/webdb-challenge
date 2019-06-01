540dc2aff3601a36ec9d5d54e45ba042590103e0
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-01 13:34:17 -0500

    added endpoints for getting projects

--- index.js
+++ index.js
@@ -16,8 +16,49 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-//server
+//~~~~~~~~~~~~~~~~~server~~~~~~~~~
 const port = 6492;
 server.listen(port, function() {
   console.log(`\n=== Webdb API Listening on http://localhost:${port} ===\n`);
 });
+
+//~~~~~~~~~~~~~~~~~~~~~helper functions~~~~~~
+//SELECT * from projects
+function findAll(){
+    return db('projects')
+  }
+  
+  //select * from project where id = id
+  function findById(id){
+    return db('projects').where({ id }).first()
+  }
+
+//~~~~~~~~~~~~~~~~~~~ endpoints here~~~~~~~~~~~~~
+server.get('/api', (req, res) => {
+    res.send(`
+    <h2>
+        WEBDB Sprint 
+    </h2>`)
+  })
+  
+  server.get('/api/projects', async (req, res) => {
+    try{
+        const projectList = await findAll();
+        res.status(200).json(projectList)
+    } catch (error) {
+        res.status(500).json({
+            message: 'The projects list could not be retrieved.'
+        })
+    }
+  })
+
+  server.get('/api/projects/:id', async (req, res) => {
+      try{
+          const project = await findById(req.params.id)
+          res.status(200).json(project)
+      } catch (error) {
+          res.status(500).json({
+              message:"The project could not be retrieved."
+          })
+      }
+  })
